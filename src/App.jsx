b4b0709a2bb243cbca252ec3f38226b3da4eab12
Dhruv Roy <dhruvakr9001@gmail.com>
2024-05-23 17:33:54 +0530

    feat:zed logo page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import StarterPage from './components/StarterPage'
 import WebUserHome from './components/WebUserHome';
+import FaviconZed from './components/FavImg';
+
 import { document } from 'postcss';
 
 import {
@@ -19,6 +21,7 @@ function App() {
         <Route exact path="/" element={<StarterPage hide="hidden" bgrang="bg-black" />}/>
         <Route exact path="/guest_login" element={<StarterPage hide="nothidden" bgrang="bg-[#242d34]" />}/>
         <Route exact path="/guest_user_home"  element={<WebUserHome/>}/>
+        <Route exact path="/zed-logo"  element={<FaviconZed/>}/>
       </Routes>
       </Router>
     </>
---
+++ src/components/FavImg.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FaviconZed = () => {
+  return (
+    <div className='min-w-[100vw] min-h-[100vh] bg-slate-500 flex justify-center items-center'>
+      <img src="zedlogo.jpg" style={{width : '150px'}} alt="Image not found" />
+    </div>
+  )
+}
+
+export default FaviconZed
