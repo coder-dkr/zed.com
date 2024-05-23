e4791d72067be52eb7eef1699a4550e60c60e11d
Dhruv Roy <dhruvakr9001@gmail.com>
2024-05-24 04:19:57 +0530

    feat:fix test 2

--- src/components/WebUserHome.jsx
+++ src/components/WebUserHome.jsx
@@ -193,7 +193,8 @@ useEffect(() => {
     
     const ActualPostStreamRef = useRef();
     function addpost(){
-        justforstoring.current.src = userPostImgLinkRef.current.files[0];
+        justforstoring.current.src = URL.createObjectURL(userPostImgLinkRef.current.files[0])
+       
         var userDetailData = JSON.parse(localStorage.getItem("userDetails"))
         if(userDetailData === null){
             var Apost ={
@@ -461,7 +462,7 @@ const time2 = Date.now()
                             <span  onMouseOut={socialItemStyleOff}   onMouseOver={socialItemStyleOn}  className="web-socialItem text-[#6e7378] flex items-center justify-center w-fit cursor-pointer">
                                 <input type="file"  id="userPostImgLink" className='hidden' ref={userPostImgLinkRef}/>
                                 <input type="file" src="" className='hidden' ref={justforstoring}/>
-                                <label htmlFor="userPostImgLink" className='cursor-pointer'>
+                                <label htmlFor="userPostImgLink" className='cursor-pointer' >
                                     <span className="siBg w-7 h-7  flex justify-center items-center rounded-full">
                                         <svg viewBox="0 0 24 24" aria-hidden="true" className="w-5 fill-[#1b92e3] r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-z80fyv r-19wmn03"><g><path d="M3 5.5C3 4.119 4.119 3 5.5 3h13C19.881 3 21 4.119 21 5.5v13c0 1.381-1.119 2.5-2.5 2.5h-13C4.119 21 3 19.881 3 18.5v-13zM5.5 5c-.276 0-.5.224-.5.5v9.086l3-3 3 3 5-5 3 3V5.5c0-.276-.224-.5-.5-.5h-13zM19 15.414l-3-3-5 5-3-3-3 3V18.5c0 .276.224.5.5.5h13c.276 0 .5-.224.5-.5v-3.086zM9.75 7C8.784 7 8 7.784 8 8.75s.784 1.75 1.75 1.75 1.75-.784 1.75-1.75S10.716 7 9.75 7z"></path></g></svg>
                                     </span></label>
