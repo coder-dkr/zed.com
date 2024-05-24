1ce3b0d4872c5e5dd798008279ab4f3c0567f1f8
Dhruv Roy <dhruvakr9001@gmail.com>
2024-05-24 23:49:54 +0530

    fix:client id

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Auth0Provider
       domain="dev-uuelf6xmfnzounfs.us.auth0.com"
-      clientId="793745749916-jvpq4jceu5vo0te946sc7peqs3hkfjgs.apps.googleusercontent.com" 
+      clientId="YEM84E0Thxu98P7eaQ6MjFMyaXtOEycy" 
       authorizationParams={{
         redirect_uri: window.location.origin
       }}
